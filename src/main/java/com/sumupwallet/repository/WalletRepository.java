bd8d2f64053a822dd1148b99e8e64ec1e8dd7a53
Peter <guitaronline89@gmail.com>
2024-12-23 16:15:20 +0200

    Introducing TransactionService TransactionController

--- src/main/java/com/sumupwallet/repository/WalletRepository.java
+++ src/main/java/com/sumupwallet/repository/WalletRepository.java
@@ -1,9 +1,8 @@
 package com.sumupwallet.repository;
 
-import com.sumupwallet.model.Transaction;
+
 import com.sumupwallet.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,17 +12,4 @@ import java.util.UUID;
 public interface WalletRepository extends JpaRepository<Wallet, UUID> {
     List<Wallet> findWalletByUserId(UUID userId);
 
-    @Query("""
-        SELECT t FROM Transaction t
-        JOIN Wallet w where w.id = :walletId
-        AND t.type = "DEPOSIT"
-        """)
-    List<Transaction> getAllDepositTransactions(UUID walletId);
-
-    @Query("""
-        SELECT t FROM Transaction t
-        JOIN Wallet w where w.id = :walletId
-        AND t.type = "WITHDRAWAL"
-        """)
-    List<Transaction> getAllWithdrawalTransactions(UUID walletId);
 }
