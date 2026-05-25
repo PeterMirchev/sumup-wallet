6a612f3db968e79653490c84f5dcdb0e78813a96
Peter <guitaronline89@gmail.com>
2026-05-25 10:22:33 +0300

    refactoring

--- src/main/java/com/sumupwallet/service/EmailService.java
+++
@@ -1,6 +0,0 @@
-package com.sumupwallet.service;
-
-public interface EmailService {
-
-    void sendEmail(String to, String subject, String body);
-}
--- src/main/java/com/sumupwallet/service/impl/UserServiceImpl.java
+++ src/main/java/com/sumupwallet/service/impl/UserServiceImpl.java
@@ -2,7 +2,6 @@ package com.sumupwallet.service.impl;
 
 import com.sumupwallet.exception.ResourceAlreadyExistException;
 import com.sumupwallet.exception.ResourceNotFoundException;
-import com.sumupwallet.service.EmailService;
 import com.sumupwallet.utils.mapper.UserMapper;
 import com.sumupwallet.model.User;
 import com.sumupwallet.repository.UserRepository;
@@ -13,8 +12,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
-
-import static com.sumupwallet.utils.CommonMessages.*;
 import static com.sumupwallet.utils.ExceptionMessages.*;
 
 @Service
@@ -22,13 +19,11 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-    private final EmailService emailService;
 
-    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, EmailService emailService) {
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
 
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
-        this.emailService = emailService;
     }
 
     @Override
@@ -40,8 +35,6 @@ public class UserServiceImpl implements UserService {
 
         User user = UserMapper.mapToUser(request);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-
-        //emailService.sendEmail(user.getEmail(), WELCOME_MESSAGE, SUCCESSFULLY_REGISTERED_EMAIL_BODY.formatted(request.getFirstName()));
 
         return userRepository.save(user);
     }
@@ -79,9 +72,5 @@ public class UserServiceImpl implements UserService {
                 .orElseThrow(() -> new ResourceNotFoundException(USER_WITH_ID_NOT_FOUND.formatted(id)));
 
         userRepository.delete(user);
-
-        if (userRepository.findById(id).isEmpty()) {
-            emailService.sendEmail(user.getEmail(), ACCOUNT_DEACTIVATION, SUCCESSFULLY_DELETED_EMAIL_BODY.formatted(user.getFirstName()));
-        }
     }
 }
